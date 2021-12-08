17f039602cdb896e764dd768f967345cd2e35f0c
Randi Firmansyah <randykelvin26@gmail.com>
2021-12-08 13:17:37 +0700

    Throttling API using Redis

--- routes/products.js
+++ routes/products.js
@@ -13,6 +13,42 @@ const redis = new Redis({
   port: 6379,
 });
 
+// Throttling API using Redis
+const APIKey = "A3ci6zhjqxrzrivlqxc2n11uo98voivh7pnf7cziv89xojrl68f";
+const rateLimit = 10;
+const longTime = 60000;
+let count = 0;
+let isLimited = false;
+const messageLimited = {
+  error: 429,
+  message: `API rate limit exceeded`,
+  command: `Please wait 1 minute`,
+};
+
+// Throttling check
+const checkLimitRate = async () => {
+  if (!isLimited) {
+    count += 1;
+    redis.set(`key:${APIKey}`, count);
+    let cek = await redis.get(`key:${APIKey}`);
+    console.log(count);
+    if (cek > rateLimit) {
+      isLimited = true;
+      count = 0;
+      setTimeout(() => (isLimited = false), longTime);
+    }
+  }
+};
+
+// check isLimited
+function checkIsLimit() {
+  checkLimitRate();
+  if (isLimited) {
+    return true;
+  }
+  return false;
+}
+
 // variable caches
 let cacheEntry;
 
@@ -28,21 +64,28 @@ const cache = async () => {
 
 // getAll
 router.get("/", async (req, res) => {
-  if (cacheEntry != null) {
-    res.json(cacheEntry);
-  } else {
-    const products = await Product.findAll();
-    redis.set("key:products", JSON.stringify(products));
-    cache();
-    res.json(products);
+  if (checkIsLimit()) res.json(messageLimited);
+  else {
+    if (cacheEntry != null) {
+      console.log("get data from cache");
+      res.json(cacheEntry);
+    } else {
+      const products = await Product.findAll();
+      redis.set("key:products", JSON.stringify(products));
+      cache();
+      res.json(products);
+    }
   }
 });
 
 // getByID
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const product = await Product.findByPk(id);
-  res.json(product);
+  if (checkIsLimit()) res.json(messageLimited);
+  else {
+    const id = req.params.id;
+    const product = await Product.findByPk(id);
+    res.json(product);
+  }
 });
 
 // Create
